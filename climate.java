7f271dc79b4c83843bf1477199ccde0ad913bfa9
ashutosh15017 <ashutosh15017@iiitd.ac.in>
2016-11-07 02:19:42 +0530

    Add files via upload
    added random sleep timers, and have to work on terminating thread

--- climate.java
+++ climate.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
@@ -6,41 +7,85 @@ import java.util.concurrent.BlockingQueue;
 public class climate {
 
 
-    private static BlockingQueue <Float> queueTemp = new ArrayBlockingQueue<>(510);
+    private static BlockingQueue <Float> queue1 = new ArrayBlockingQueue<>(510);
+    private static BlockingQueue <Float> queue2 = new ArrayBlockingQueue<>(510);
+    private static BlockingQueue <Float> queue3 = new ArrayBlockingQueue<>(510);
+
+
     static float minValue = 10000;
     static float maxValue = -10000;
+    static float sum = 0;
+    static float avg = 0;
+    static float count = 0 ;
+    static volatile boolean runVar = true;
+
     static String result = Readfile("src/temperature.txt");
     static String value[] = result.split("\n");
 
 
     public static void main(String[] args)throws InterruptedException {
 
-        Thread t1 = new Thread(new Runnable() {
+        Thread tProdtemp = new Thread(new Runnable() {
             @Override
             public void run() {
                 producerTemp();
             }
         });
-        Thread t2 = new Thread(new Runnable() {
+
+
+        Thread tConmin = new Thread(new Runnable() {
             @Override
             public void run() {
                 consumerMin();
             }
         });
 
-        Thread t3 = new Thread(new Runnable() {
+        Thread tConmax = new Thread(new Runnable() {
             @Override
             public void run() {
                 consumerMax();
             }
         });
 
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        t3.join();
+        Thread tConavg = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                consumerAvg();
+            }
+        });
+
+        tProdtemp.start();
+        tConmin.start();
+        tConmax.start();
+        tConavg.start();
+
+        tProdtemp.join();
+        tConmin.join();
+        tConmax.join();
+        tConavg.join();
+
+        // HUMIDITY
+
+         queue1.clear();
+         queue2.clear();
+         queue3.clear();
+         String result = Readfile("src/humidity.txt");
+         String value[] = result.split("\n");
+         minValue = 10000;
+         maxValue = -10000;
+         sum = 0;
+         avg = 0;
+         count = 0 ;
+
+         tProdtemp.start();
+         tConmin.start();
+         tConmax.start();
+         tConavg.start();
+
+         tProdtemp.join();
+         tConmin.join();
+         tConmax.join();
+         tConavg.join();
 
 
 
@@ -50,76 +95,108 @@ public class climate {
 
     public  static void producerTemp ()
     {
+        int counter=0;
+        Random random = new Random();
+        int num = random.nextInt(6)+1;
 
-        try {
+            try {
 
-            Thread.sleep(1);
-            for (int i = 0 ; i <504 ; i++)
-            {
-                try
+                Thread.sleep(1);
+                for (int i = 0 ; i <504 ; i++)
                 {
-                    queueTemp.put(Float.parseFloat(value[i]));
+                    try
+                    {
+                        queue1.put(Float.parseFloat(value[i]));
+                        queue2.put(Float.parseFloat(value[i]));
+                        queue3.put(Float.parseFloat(value[i]));
+                    }
+
+                    catch (NumberFormatException e)
+                    {
+
+                    }
+
+                }
+                while (runVar)
+                {
+                    counter++;
+                    float minValue = consumerMin();
+                    System.out.println("Min Value: " + minValue);
+
+                    float maxValue = consumerMax();
+                    System.out.println("Max Value: " + maxValue);
+
+                    float avgValue = consumerAvg();
+                    System.out.println("Avg Value: " + avgValue);
+                    System.out.println();
+
+
+                    if (counter == 499)
+                        runVar = shutdown();
                 }
 
-                catch (NumberFormatException e)
-                {
-
-                }
 
             }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+
+
 
 
 
 
     }
 
-    public static void consumerMin ()
+
+
+    public static float consumerMin ()
     {
+        Random random = new Random();
+        int num = random.nextInt(3);
+
         while (true)
         {
             try {
 
-                Thread.sleep(100);
-                float value = queueTemp.take();
-
+                Thread.sleep(1);
+                float value = queue1.take();
+                System.out.println(value);
                 if (value <= minValue)
                 {
                     minValue = value;
                 }
-                System.out.println("Min value: " + minValue);
-                System.out.println("Queue size (min): " + queueTemp.size());
+//                System.out.println("Min value: " + minValue);
+//                System.out.println("Queue size (min): " + queue1.size());
 
             }
             catch (Exception e)
             {
                 e.printStackTrace();
             }
-
+            return minValue;
         }
     }
 
-    public static void consumerMax ()
+    public static float consumerMax ()
     {
+        Random random = new Random();
+        int num = random.nextInt(3);
+
         while (true)
         {
             try {
 
-                Thread.sleep(100);
-                float value = queueTemp.take();
-
-
-
+                Thread.sleep(1);
+                float value = queue2.take();
                 if (value >= maxValue)
                 {
                     maxValue = value;
                 }
-                System.out.println("Max value: " + maxValue);
-                System.out.println("Queue size (max): " + queueTemp.size());
+//                System.out.println("Max value: " + maxValue);
+//                System.out.println("Queue size (max): " + queue2.size());
 
 
             }
@@ -127,8 +204,43 @@ public class climate {
             {
                 e.printStackTrace();
             }
-
+            return maxValue;
         }
+    }
+
+    public static float consumerAvg ()
+    {
+        Random random = new Random();
+        int num = random.nextInt(3);
+
+        while (true)
+        {
+            try {
+
+                Thread.sleep(1);
+                count++;
+                float value = queue3.take();
+                sum+=value;
+                avg = sum/count;
+//                System.out.println("Sum: " + sum);
+//                System.out.println("Average value: " + avg);
+//                System.out.println("Queue size (avg): " + queue2.size());
+
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return avg;
+        }
+    }
+
+    public static boolean shutdown()
+    {
+        System.out.println("shut down");
+        runVar = false;
+        return runVar;
     }
 
     static String Readfile (String fname)
